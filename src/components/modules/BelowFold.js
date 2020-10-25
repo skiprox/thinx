7fdcfa4b80f1314a85757a2d49587b349e2a080c
skiprox <seanmichaelscanlan@gmail.com>
2020-10-25 14:25:15 -0700

    Updating below fold section with images and text, basic layout for desktop and mobile

---
+++ src/components/modules/BelowFold.js
@@ -0,0 +1,37 @@
+export default function BelowFold() {
+  return (
+    <article className="BelowFold">
+      <div className="Container Container--thin flex justify-between items-center">
+        <div className="BelowFold__item">
+          <img className="BelowFold__image BelowFold__image--medium" src="/images/bottom/thinx_productpage-04.jpg"></img>
+          <a href="#" className="BelowFold__link">Shop Hiphugger</a>
+          <p className="BelowFold__description">heavy days</p>
+        </div>
+        <div className="BelowFold__item">
+          <img className="BelowFold__image BelowFold__image--small" src="/images/bottom/thinx_productpage-03.jpg"></img>
+          <a href="#" className="BelowFold__link">Shop Hiphugger</a>
+          <p className="BelowFold__description">heavy days</p>
+        </div>
+      </div>
+      <div className="Container Container--thin flex justify-center items-center">
+        <div className="BelowFold__item">
+          <img className="BelowFold__image BelowFold__image--medium" src="/images/bottom/thinx_productpage-05.jpg"></img>
+          <a href="#" className="BelowFold__link">Shop Hiphugger</a>
+          <p className="BelowFold__description">heavy days</p>
+        </div>
+      </div>
+      <div className="Container Container--thin flex justify-between items-center">
+        <div className="BelowFold__item">
+          <img className="BelowFold__image BelowFold__image--medium" src="/images/bottom/thinx_productpage-07.jpg"></img>
+          <a href="#" className="BelowFold__link">Shop Hiphugger</a>
+          <p className="BelowFold__description">heavy days</p>
+        </div>
+        <div className="BelowFold__item">
+          <img className="BelowFold__image BelowFold__image--small" src="/images/bottom/thinx_productpage-08.jpg"></img>
+          <a href="#" className="BelowFold__link">Shop Hiphugger</a>
+          <p className="BelowFold__description">heavy days</p>
+        </div>
+      </div>
+    </article>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from 'components/Layout';
 import Header from 'components/site/Header';
 import Footer from 'components/site/Footer';
 import Hero from 'components/modules/Hero';
+import BelowFold from 'components/modules/BelowFold';
 
 export default function Home(context) {
   return (
     <Layout preview={false}>
       <Header />
       <Hero />
+      <BelowFold />
       <Footer />
     </Layout>
   )
