2f4eb3bd527b203e5914b388b37a18f32f63d464
skiprox <seanmichaelscanlan@gmail.com>
2020-10-26 09:24:09 -0700

    Work on the product size dropdown for desktop and mobile

--- src/components/modules/ProductSale.js
+++ src/components/modules/ProductSale.js
@@ -4,6 +4,8 @@ import classnames from 'classnames';
 export default function ProductSale() {
   const [productColor, setProductColor] = useState('black');
   const [productQuantity, setProductQuantity] = useState(1);
+  const [sizeSelectorOpen, setSizeSelectorOpen] = useState(false);
+  const [size, setSize] = useState('Size');
   return (
     <div className="Product Product__sale flex flex-column justify-center">
       <h3 className="Product__cost">$35.00</h3>
@@ -53,11 +55,49 @@ export default function ProductSale() {
       </ul>
       <ul className="Product__size">
         <li className="Product__size-button-wrapper">
-          <button className="Product__size-button flex justify-between">
-            <span>size</span>
-            <span className="Product__size-chevron">V</span>
+          <button
+            className={classnames("Product__size-button flex justify-between", {
+              active: sizeSelectorOpen
+            })}
+            onClick={() => {
+              setSizeSelectorOpen(!sizeSelectorOpen);
+            }}
+          >
+            <span>{size}</span>
+            <span className="Product__size-chevron">‹</span>
           </button>
         </li>
+        <ul className={classnames("Product__size-selector", {
+          active: sizeSelectorOpen
+        })}>
+          <li className="Product__size-select">
+            <button
+              className="Product__size-select-button"
+              onClick={() => {
+                setSize('small');
+                setSizeSelectorOpen(false);
+              }}
+            >small</button>
+          </li>
+          <li className="Product__size-select">
+            <button
+              className="Product__size-select-button"
+              onClick={() => {
+                setSize('medium');
+                setSizeSelectorOpen(false);
+              }}
+            >medium</button>
+          </li>
+          <li className="Product__size-select">
+            <button
+              className="Product__size-select-button"
+              onClick={() => {
+                setSize('large');
+                setSizeSelectorOpen(false);
+              }}
+            >large</button>
+          </li>
+        </ul>
       </ul>
       <div className="Product__question">
         <a href="#" className="Product__question-link">what's my size?</a>
@@ -66,6 +106,14 @@ export default function ProductSale() {
       <div className="Product__additional tc">
         <a href="#" className="Product__additional-link">build a cycle set and save up to 20%</a>
       </div>
+      <div
+        className={classnames('Product__size-selector-overlay', {
+          active: sizeSelectorOpen
+        })}
+        onClick={() => {
+          setSizeSelectorOpen(false);
+        }}
+      ></div>
     </div>
   );
 };
